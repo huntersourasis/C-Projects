385474d9b669e7021251882651687f9239eb1924
Sourasis Maity <128131302+huntersourasis@users.noreply.github.com>
2025-07-22 19:33:48 +0530

    Update hcf.c

--- hcf.c
+++ hcf.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main() {
-  int num1 , num2 , temp;
-  printf("Enter the first number : ");
-  scanf("%d" , &num1);
-  printf("Enter the second number : ");
-  scanf("%d" , &num2);
-  num1 = abs(num1);
-  num2 = abs(num2);
-  while (num2 % num1 > 0) {
-    temp = num1;
-    num1 = num2 % num1;
-    num2 = temp;
-  }
-  printf("%d" , num1);
-  return 0;
+    int num1;
+    printf("Num1 : ");
+    scanf("%d" , &num1);
+    int num2;
+    printf("\nNum2 : ");
+    scanf("%d" , &num2);
+    int st_num1 = num1  , st_num2 = num2 ;
+    num1 = num1 != 0 ? abs(num1) : printf("\nNum1 Should be an non-zero Number.");
+    num2 = num2 != 0 ?  abs(num2) : printf("\nNum2 Should be an non-zero Number.") ;
+    int temp;
+    while (num2 % num1 > 0)
+    {
+        temp = num1;
+        num1 =  num2 % num1;
+        num2 = temp;
+    }
+    printf("The HCF of %d and %d is : %d" , st_num1 , st_num2 , num1 );
+    return 0;
 }
